91d106ffb9e9a9365f976cd4149e5232b976796f
sasaplus1 <sasaplus1@gmail.com>
2020-01-13 02:37:38 +0900

    prototyping

---
+++ src/manifest.js
@@ -0,0 +1,30 @@
+module.exports = {
+  manifest_version: 2,
+  name: 'nochmos',
+  version: '0.0.0',
+  description: '',
+  icons: {
+    '16': 'icon_32.png',
+    '48': 'icon_32.png',
+    '128': 'icon_32.png'
+  },
+  browser_action: {
+    default_icon: {
+      '16': 'icon_32.png',
+      '24': 'icon_32.png',
+      '32': 'icon_32.png'
+    },
+    default_title: 'nochmos',
+    default_popup: 'popup.html'
+  },
+  commands: {
+    _execute_browser_action: {
+      suggested_key: {
+        default: 'Ctrl+T',
+        mac: 'MacCtrl+T'
+      },
+      description: 'open popup'
+    }
+  },
+  permissions: ['bookmarks', 'history', 'tabs', 'topSites']
+};
