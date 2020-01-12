638dde3b6a934e4a254d89fe03927e09ce185325
sasaplus1 <sasaplus1@gmail.com>
2020-01-12 17:16:14 +0900

    add rollup plugin

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,18 @@
+import commonJs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
 const config = [];
 
 config.push({
-  input: './src/popup.tsx',
+  input: './src/popup.ts',
   output: {
     file: './lib/popup.js',
     format: 'iife',
     name: 'popup',
     sourcemap: true
   },
-  plugins: [typescript(), nodeResolve()]
+  plugins: [typescript(), nodeResolve(), commonJs()]
 });
 
 export default config;
