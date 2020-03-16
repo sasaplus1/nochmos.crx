b5df9fa9b6dd3b81f51f2ef110eb2c3079522384
sasaplus1 <sasaplus1@gmail.com>
2020-03-17 01:22:24 +0900

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,15 @@ import commonJs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 
-const config = [];
-
-config.push({
-  input: './src/popup.ts',
-  output: {
-    file: './lib/popup.js',
-    format: 'iife',
-    name: 'popup',
-    sourcemap: true
-  },
-  plugins: [typescript(), nodeResolve(), commonJs()]
-});
-
-export default config;
+export default [
+  {
+    input: './src/popup.ts',
+    output: {
+      file: './lib/popup.js',
+      format: 'iife',
+      name: 'popup',
+      sourcemap: true
+    },
+    plugins: [typescript(), nodeResolve(), commonJs()]
+  }
+];
