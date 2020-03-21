a8baf579c5a7d5b301dfd0bf2eff4fc7cb76fc19
sasaplus1 <sasaplus1@gmail.com>
2020-03-21 16:24:07 +0900

    add storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,8 @@
 module.exports = {
-  addons: ['@storybook/preset-typescript'],
-  stories: ['../src/**/*.stories.[tj]s?(x)'],
+  addons: [
+    '@storybook/preset-typescript',
+    '@storybook/addon-actions/register',
+    '@storybook/addon-knobs/register'
+  ],
+  stories: ['../src/**/*.stories.[tj]s?(x)']
 };
