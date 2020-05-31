3968f06128c7bd8d5fce653da2c3e9d79ebefafa
sasaplus1 <sasaplus1@gmail.com>
2020-05-31 17:40:22 +0900

    add extension icon
    icon got from: https://icooon-mono.com/15878-%e3%83%90%e3%82%a4%e3%82%af%e3%82%a2%e3%82%a4%e3%82%b3%e3%83%b3/

--- src/manifest.js
+++ src/manifest.js
@@ -4,20 +4,16 @@ module.exports = {
   version: '0.0.0',
   description: 'fuzzy find from some sources',
   icons: {
-    '16': 'icon_32.png',
-    '48': 'icon_32.png',
-    '128': 'icon_32.png'
+    '16': 'icon_16.png',
+    '32': 'icon_32.png',
+    '64': 'icon_64.png',
+    '128': 'icon_128.png'
   },
   background: {
     persistent: false,
     scripts: ['background.js']
   },
   browser_action: {
-    default_icon: {
-      '16': 'icon_32.png',
-      '24': 'icon_32.png',
-      '32': 'icon_32.png'
-    },
     default_title: 'nochmos',
     default_popup: 'popup.html'
   },
